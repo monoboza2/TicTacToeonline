16af7824b25034f9db595ece7b8b006d30ddf047
monoboza2 <thanathon.first2509@gmail.com>
2022-04-03 21:05:20 +0700

    Logout

--- app/src/main/java/th/ac/kmutnb/tictactoe2/MainActivity.java
+++ app/src/main/java/th/ac/kmutnb/tictactoe2/MainActivity.java
@@ -2,7 +2,11 @@ package th.ac.kmutnb.tictactoe2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView Logoutbtn=findViewById(R.id.imgview);
+
+        Logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplication(), "Logout", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this,Login.class));
+            }
+        });
     }
 }
