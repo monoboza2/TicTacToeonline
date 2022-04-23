143cbbc6be11a32e12bc79dc8ac3ad4d5dc6bb4c
monoboza2 <thanathon.first2509@gmail.com>
2022-04-23 21:14:06 +0700

    test online

---
+++ app/src/main/java/th/ac/kmutnb/tictactoe2/Test.java
@@ -0,0 +1,14 @@
+package th.ac.kmutnb.tictactoe2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
